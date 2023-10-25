af205b35d99de074d058bf6e3ee0fe769398706f
TyJacalan <muzuuu@protonmail.com>
2023-10-25 20:41:52 +0800

    added data models

---
+++ src/lib/constants/usersModel.js
@@ -0,0 +1,36 @@
+const usersModel = [
+    {
+        user_id: 1,
+        username: "Admin",
+        first_name: "",
+        last_name: "",
+        email: "admin@gmail.com",
+        role: "admin",
+    },
+    {
+        user_id: 2,
+        username: "Ty",
+        first_name: "Ty",
+        last_name: "Jacalan",
+        email: "tyrone.jacalan@protonmail.com",
+        role: "client",
+    },
+    {
+        user_id: 3,
+        username: "maeci",
+        first_name: "Mae",
+        last_name: "Cimafranca",
+        email: "cimafrancamae@gmail.com",
+        role: "client",
+    },
+    {
+        user_id: 2,
+        username: "Ian",
+        first_name: "Ian",
+        last_name: "Rebulado",
+        email: "eilrebulado@gmail.com",
+        role: "client",
+    },
+];
+
+export default usersModel;
