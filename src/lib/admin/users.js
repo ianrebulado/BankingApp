b1629762f6af6b862b7133c899958aade3d1bbed
TyJacalan <muzuuu@protonmail.com>
2023-10-25 21:35:35 +0800

    Add filter_users_by_name function

--- src/lib/admin/users.js
+++ src/lib/admin/users.js
@@ -2,6 +2,11 @@ export function createUser(){
 
 }
 
-export function filter_users_by_name(){
-    
+export function filter_users_by_name(users, name){
+    const matchedUser = users.filter(user =>
+        user.first_name.toLowerCase().includes(name.toLowerCase()) ||
+        user.last_name.toLowerCase().includes(name.toLowerCase())
+    );
+
+    return matchedUser[0];
 }
