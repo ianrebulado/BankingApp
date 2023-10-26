e12f4c04eb3c731fc7c04c0509d4a4c915747539
TyJacalan <muzuuu@protonmail.com>
2023-10-26 21:06:59 +0800

    Change component name to 'Button'

--- src/components/Global/Button/Button.jsx
+++ src/components/Global/Button/Button.jsx
@@ -1,4 +1,4 @@
-export default function Buttons({type, text, handleClick}) {
+export default function Button({type, text, handleClick}) {
   return (
     <button type={type} className={`${text.toLowerCase().replace(/\s+/g, '-')}-btn`} onClick={() => handleClick}><span>{text}</span></button>
   )
