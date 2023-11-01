8c4ad0f23ba6201831b276b0d0d39b58104603b9
cimafrancamae <cimafrancamae@gmail.com>
2023-11-01 10:58:34 +0800

    Added pagination to table component, added name formatter function, updated admin dashboard

--- src/components/Global/Table/Table.jsx
+++ src/components/Global/Table/Table.jsx
@@ -1,25 +1,44 @@
+import { useState } from 'react'
+import { formatName } from '../../../lib/utils/formatName';
 import '../../../styles/css/styles.css'
 
-const Table = ({data, columns}) => {
+const Table = ({data, columns, itemsPerPage}) => {
+    const [currentPage, setCurrentPage] =  useState(1);
+    const totalPages = Math.ceil(data.length / itemsPerPage);
+    const handlePageChange = (page) => {
+        setCurrentPage(page)
+    }
+
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const currentData = data.slice(startIndex, endIndex)
+
     return (
-        <table className="table-container">
-            <thead>
-                <tr>
-                    {columns.map((column, index) => (
-                        <th key={index}>{column}</th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {data.map((item, rowIndex) => (
-                    <tr key={rowIndex}>
-                        {columns.map((column, colIndex) => (
-                            <td key={colIndex}>{item[column]}</td>
+        <>
+            <table className="table-container">
+                <thead>
+                    <tr>
+                        {columns.map((column, index) => (
+                            <th key={index}>{formatName(column)}</th>
                         ))}
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {currentData.map((item, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {columns.map((column, colIndex) => (
+                            <td key={colIndex}>{item[column]}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div className="pagination">
+                <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
+                <span>{currentPage} of {totalPages}</span>
+                <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>Next</button>
+            </div>
+        </>
     )
 } 
 
