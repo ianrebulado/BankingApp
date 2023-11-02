40b750dca280ac237b608726a871e07ad6cbadac
cimafrancamae <cimafrancamae@gmail.com>
2023-11-02 13:40:35 +0800

    Update BudgetApp.jsx

--- src/pages/BudgetApp/BudgetApp.jsx
+++ src/pages/BudgetApp/BudgetApp.jsx
@@ -1,20 +1,38 @@
 import React, { useState } from 'react';
 import { Button, Card, Table } from '../../components';
+import { FileEdit, FileX } from 'lucide-react';
+import expensesModel from '../../lib/constants/expensesModel';
 
 function BudgetApp(props) {
 
     const [userId, setUserId] = useState('');
     const [expenses, setExpenses] = useState([]);
 
+    const data = expensesModel
+
+    const handleEditClick = () => {
+
+    }
+
+    const handleDeleteClick = () => {
+
+    }
+
     return (
         <>
             <div className='expenses-container'>
                 <div className="card-container">
-                    <Card label={'Current Balance'} value={'100,000'} />
-                    <Card label={'Total Expenses'} value={'10,000'} />
+                    <Card title={'Current Balance'} content={'100,000'} />
+                    <Card title={'Total Expenses'} content={'10,000'} />
                 </div>
                 <Button type={'submit'} text={'Add Expense'} handleClick={()=>{}} />
-                <Table data={[]} columns={['ID', 'Date', 'Description', 'Amount']} />
+                <Table data={[]} columns={['ID', 'Date', 'Description', 'Amount']} 
+                    actions={(item) => (
+                        <>
+                            <FileEdit onClick={() => handleEditClick(item)} />
+                            <FileX onClick={() => handleDeleteClick(item)} />
+                        </>
+                    )} />
             </div>
         </>
     );
