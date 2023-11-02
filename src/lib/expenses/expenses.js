85e8b1011fc9badbd9a496a43a6553e175032e77
cimafrancamae <cimafrancamae@gmail.com>
2023-11-02 22:23:46 +0800

    added addexpense form

--- src/lib/expenses/expenses.js
+++ src/lib/expenses/expenses.js
@@ -1,9 +1,26 @@
+import generateId from '../utils/generateId';
+
 export function getExpense(expenseId){
 
 }
 
-export function addExpense(userId){
+export function addExpense(newExpense){
+    const expenseId = generateId();
+    const createdOn = new Date();
+    const updatedOn = createdOn;
+    
+    newExpense = {
+        ...newExpense, 
+        expense_id: expenseId, 
+        created_on: createdOn, 
+        updated_on: updatedOn 
+    }
 
+    const data = localStorage.getItem('expenses');
+    const expenses = data ? JSON.parse(data) : [];
+
+    expenses.push(newExpense);
+    localStorage.setItem('expenses', JSON.stringify(expenses))
 }
 
 export function updateExpense(expenseId){
@@ -12,4 +29,14 @@ export function updateExpense(expenseId){
 
 export function deleteExpense(expenseId){
     
+}
+
+export function storeInitialExpenses(data){
+    localStorage.setItem('expenses', JSON.stringify(data));
+}
+
+export function getUserExpenses(userId){
+    const expenses = localStorage.getItem('expenses');
+    const userExpenses = JSON.parse(expenses).filter((item) => item.user_id === userId)
+    return userExpenses
 }
--- src/pages/BudgetApp/BudgetApp.jsx
+++ src/pages/BudgetApp/BudgetApp.jsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card, Table } from '../../components';
+import { Button, Card, Header, Modal, Table } from '../../components';
 import { FileEdit, FileX } from 'lucide-react';
 import expensesModel from '../../lib/constants/expensesModel';
+import { storeInitialExpenses, getUserExpenses } from '../../lib/expenses/expenses';
+import AddExpenseForm from './AddExpenseForm';
 
-function BudgetApp(props) {
+function BudgetApp() {
 
-    const [userId, setUserId] = useState('');
+    const userId = 'u-l2hckqwf1p';
     const [expenses, setExpenses] = useState([]);
 
     const columns = ['expense_id', 'created_on', 'description', 'amount'];
-    const data = expensesModel.map((item) => {
+    const data = expenses.map((item) => {
         const {expense_id, created_on, description, amount} = item;
         return {expense_id, created_on, description, amount}
     })
 
     useEffect(()=>{
-        setExpenses(data);
+        setExpenses(
+            getUserExpenses(userId)
+        );
+
+        storeInitialExpenses(expensesModel);
     }, [])
+
+    const [showModal, setShowModal] = useState(false)
+    const handleAddClick = () => {
+        setShowModal(true)
+    }
 
     const handleEditClick = () => {
 
@@ -29,12 +40,20 @@ function BudgetApp(props) {
     return (
         <>
             <div className='expenses-container'>
+                <Header user={'Mae'} />
                 <div className="card-container">
                     <Card title={'Current Balance'} content={'100,000'} />
                     <Card title={'Total Expenses'} content={'10,000'} />
                 </div>
-                <Button type={'submit'} text={'Add Expense'} handleClick={()=>{}} />
-                <Table data={expenses} columns={columns} itemsPerPage={3}
+                <Button type={'button'} text={'Add Expense'} handleClick={handleAddClick}/>
+                {
+                    showModal && (
+                        <Modal title={"New Expense"} onClose={()=>setShowModal(false)}>
+                            <AddExpenseForm />
+                        </Modal>
+                    )
+                }
+                <Table data={data} columns={columns} itemsPerPage={3}
                     actions={ (
                         <>
                             <FileEdit onClick={() => handleEditClick} />
