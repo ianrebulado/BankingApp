a039e84d8106ad648dfad200eb17a410c7c709cf
TyJacalan <muzuuu@protonmail.com>
2023-11-03 14:39:22 +0800

    Remove localstorage commands in addExpense

--- src/lib/expenses/expenses.js
+++ src/lib/expenses/expenses.js
@@ -1,42 +1,36 @@
-import generateId from '../utils/generateId';
+import generateId from "../utils/generateId";
 
-export function getExpense(expenseId){
+export function getExpense(expenseId) {}
 
+export function addExpense(newExpense) {
+  const expenseId = generateId();
+  const createdOn = new Date();
+  const updatedOn = createdOn;
+
+  newExpense = {
+    ...newExpense,
+    expense_id: expenseId,
+    created_on: createdOn,
+    updated_on: updatedOn,
+  };
+
+  const expenses = data ? JSON.parse(data) : [];
+
+  expenses.push(newExpense);
 }
 
-export function addExpense(newExpense){
-    const expenseId = generateId();
-    const createdOn = new Date();
-    const updatedOn = createdOn;
-    
-    newExpense = {
-        ...newExpense, 
-        expense_id: expenseId, 
-        created_on: createdOn, 
-        updated_on: updatedOn 
-    }
+export function updateExpense(expenseId) {}
 
-    const data = localStorage.getItem('expenses');
-    const expenses = data ? JSON.parse(data) : [];
+export function deleteExpense(expenseId) {}
 
-    expenses.push(newExpense);
-    localStorage.setItem('expenses', JSON.stringify(expenses))
+export function storeInitialExpenses(data) {
+  localStorage.setItem("expenses", JSON.stringify(data));
 }
 
-export function updateExpense(expenseId){
-
-}
-
-export function deleteExpense(expenseId){
-    
-}
-
-export function storeInitialExpenses(data){
-    localStorage.setItem('expenses', JSON.stringify(data));
-}
-
-export function getUserExpenses(userId){
-    const expenses = localStorage.getItem('expenses');
-    const userExpenses = JSON.parse(expenses).filter((item) => item.user_id === userId)
-    return userExpenses
+export function getUserExpenses(userId) {
+  const expenses = localStorage.getItem("expenses");
+  const userExpenses = JSON.parse(expenses).filter(
+    (item) => item.user_id === userId
+  );
+  return userExpenses;
 }
