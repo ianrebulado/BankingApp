f48cbf461258e03329360972833ffd12b39445cd
TyJacalan <muzuuu@protonmail.com>
2023-11-04 12:23:08 +0800

    Update SignUpForm validation to use local storage

--- src/lib/utils/validations.js
+++ src/lib/utils/validations.js
@@ -1,114 +1,121 @@
+import usersModel from "../constants/usersModel";
+import { fetchUsers } from "./users";
+
 export function clearValidationMessages(inputState, setInputState) {
-    const updatedInputState = inputState.map((input) => ({
-      ...input,
-      message: "",
-    }));
-    setInputState(updatedInputState);
-  }
-  
-export function validateSignUpForm(inputState, setInputState, formState, usersModel){
-
-    let isValid = true;
-
-    let updatedInputState = checkMissingValues(inputState, formState);
-
-    updatedInputState = checkDuplicate(updatedInputState, formState, usersModel);
-
-    setInputState(updatedInputState);
-
-    isValid = checkValidForm(updatedInputState);
-
-    return isValid;
+  const updatedInputState = inputState.map((input) => ({
+    ...input,
+    message: "",
+  }));
+  setInputState(updatedInputState);
 }
 
-export function validateSignInForm(inputState, setInputState, formState, usersModel){
+export function validateSignUpForm(
+  inputState,
+  setInputState,
+  formState,
+  users = fetchUsers()
+) {
+  let isValid = true;
 
-    let isValid = true;
+  let updatedInputState = checkMissingValues(inputState, formState);
 
-    const user = matchUserCredentials(formState, usersModel);
+  updatedInputState = checkDuplicate(updatedInputState, formState, users);
 
-    let updatedInputState = checkCredentials(inputState, user);
-    setInputState(updatedInputState);
+  setInputState(updatedInputState);
 
-    isValid = checkValidForm(updatedInputState);
+  isValid = checkValidForm(updatedInputState);
 
-    return isValid;
+  return isValid;
 }
 
-function checkMissingValues(inputState, formState){
-    const newInputState = inputState.map((input) => {
-        if (!formState[input.name] && input.isRequired) {
-            return { ...input, message: "This field is required" };
-        } 
-        
-        return { ...input, message: "" };
-    })
+export function validateSignInForm(
+  inputState,
+  setInputState,
+  formState,
+  usersModel
+) {
+  let isValid = true;
 
-    return newInputState;
+  const user = matchUserCredentials(formState, usersModel);
 
+  let updatedInputState = checkCredentials(inputState, user);
+  setInputState(updatedInputState);
+
+  isValid = checkValidForm(updatedInputState);
+
+  return isValid;
 }
-  
+
+function checkMissingValues(inputState, formState) {
+  const newInputState = inputState.map((input) => {
+    if (!formState[input.name] && input.isRequired) {
+      return { ...input, message: "This field is required" };
+    }
+
+    return { ...input, message: "" };
+  });
+
+  return newInputState;
+}
+
 function checkDuplicate(inputState, formState, usersModel) {
-  
-    const newInputState = inputState.map((input) => {
-      if (input.name === 'username' || input.name === 'email') {
-        if (isDuplicate(input.name, formState, usersModel)) {
-          return { ...input, message: `${input.name} already exists.` };
-        } else {
-          return { ...input, message: "" };
-        }
+  const newInputState = inputState.map((input) => {
+    if (input.name === "username" || input.name === "email") {
+      if (isDuplicate(input.name, formState, usersModel)) {
+        return { ...input, message: `${input.name} already exists.` };
+      } else {
+        return { ...input, message: "" };
       }
-      return input;
-    });
-
-    return newInputState;
-  
-  }
-  
-  function isDuplicate(property, formState, usersModel) {
-    const duplicateProperty = usersModel.find((user) => user[property] === formState[property]);
-  
-    return !!duplicateProperty;
-  }
-
-function matchUserCredentials({username, password}, users){
-
-    const user = {
-        username: null,
-        errorType: null,
     }
+    return input;
+  });
 
-    let matchedUser = users.find(user => user.username === username);
+  return newInputState;
+}
 
-    if(!matchedUser){
-        user.errorType = "user";
-    } else if(matchedUser.signed_in){
-        user.errorType = "sign-in";
-    } else if(matchedUser.password !== password){
-        user.errorType = "password";
+function isDuplicate(property, formState, usersModel) {
+  const duplicateProperty = usersModel.find(
+    (user) => user[property] === formState[property]
+  );
+
+  return !!duplicateProperty;
+}
+
+function matchUserCredentials({ username, password }, users) {
+  const user = {
+    username: null,
+    errorType: null,
+  };
+
+  let matchedUser = users.find((user) => user.username === username);
+
+  if (!matchedUser) {
+    user.errorType = "user";
+  } else if (matchedUser.signed_in) {
+    user.errorType = "sign-in";
+  } else if (matchedUser.password !== password) {
+    user.errorType = "password";
+  }
+
+  return user;
+}
+
+function checkCredentials(inputState, userCredentials) {
+  const newInputState = inputState.map((input) => {
+    if (input.name === "username" && userCredentials.errorType === "user") {
+      return { ...input, message: `Username not found.` };
+    } else if (
+      input.name === "password" &&
+      userCredentials.errorType === "password"
+    ) {
+      return { ...input, message: `Wrong password detected` };
     }
+    return { ...input, message: "" };
+  });
 
-    return user;
-
+  return newInputState;
 }
 
-function checkCredentials(inputState, userCredentials){
-    const newInputState = inputState.map((input) => {
-        if (input.name === 'username' && userCredentials.errorType === "user") {
-            return { ...input, message: `Username not found.` };
-          } else if (input.name === 'password' && userCredentials.errorType === "password") {
-            return { ...input, message: `Wrong password detected` };
-          }
-          return {...input, message: ""};
-        })
-
-    return newInputState;
+function checkValidForm(inputState) {
+  return inputState.every((input) => input.message === "");
 }
-
-function checkValidForm(inputState){
-    return inputState.every((input) => input.message === "");
-}
-  
-
-
-  
