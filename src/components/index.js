db33e17f38fd615a98b1532fadd9ead74132544a
TyJacalan <muzuuu@protonmail.com>
2023-11-04 12:40:42 +0800

    Add FormProvider export

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,9 @@ import Button from "./Global/Button/Button";
 import Card from "./Global/Card/Card";
 import Header from "./Global/Header/Header";
 import InputField from "./Global/InputField/InputField";
+import { FormProvider } from "./Global/Form/FormContext";
 import Modal from "./Global/Modal/Modal";
 import Toast from "./Global/Toast/Toast";
 import Table from "./Global/Table/Table";
 
-export { Button, Card, Header, InputField, Modal, Toast, Table };
+export { Button, Card, Header, InputField, FormProvider, Modal, Toast, Table };
