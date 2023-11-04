d760546ba6d28f82f04a183ff525ba142660f3a4
TyJacalan <muzuuu@protonmail.com>
2023-11-04 12:22:21 +0800

    Add toast import

--- src/components/index.js
+++ src/components/index.js
@@ -3,8 +3,7 @@ import Card from "./Global/Card/Card";
 import Header from "./Global/Header/Header";
 import InputField from "./Global/InputField/InputField";
 import Modal from "./Global/Modal/Modal";
+import Toast from "./Global/Toast/Toast";
 import Table from "./Global/Table/Table";
 
-export {
-    Button, Card, Header, InputField, Modal, Table
-}
+export { Button, Card, Header, InputField, Modal, Toast, Table };
