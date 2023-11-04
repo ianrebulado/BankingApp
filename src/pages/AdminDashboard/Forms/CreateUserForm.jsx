c3c78145a8ce454d00da56581f8f1188108a16fa
TyJacalan <muzuuu@protonmail.com>
2023-11-04 12:24:45 +0800

    Add functionality to close modal and show toast

---
+++ src/pages/AdminDashboard/Forms/CreateUserForm.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import { Button, Modal, InputField } from "../../../components/";
+import { FormProvider } from "../../../components/Global/Form/FormContext";
+import usersModel from "../../../lib/constants/usersModel";
+import { createUser } from "../../../lib/utils/users";
+import {
+  createUserFormInputs,
+  initialCreateUserFormState,
+} from "../../../lib/constants/globals";
+import {
+  clearValidationMessages,
+  validateSignUpForm,
+} from "../../../lib/utils/validations";
+
+export default function CreateUserForm({ setShowModal, setShowToast }) {
+  const [inputState, setInputState] = useState(createUserFormInputs);
+  const [formState, setFormState] = useState(initialCreateUserFormState);
+  function handleSubmit(e) {
+    e.preventDefault();
+    let isValidForm = false;
+
+    clearValidationMessages(inputState, setInputState);
+
+    isValidForm = validateSignUpForm(
+      inputState,
+      setInputState,
+      formState,
+      usersModel
+    );
+
+    if (isValidForm) {
+      createUser(formState);
+
+      setShowToast(true);
+      setShowModal(false);
+    } else {
+      console.log("Form is not valid");
+    }
+  }
+
+  function handleInputChange(name, value) {
+    setFormState({ ...formState, [name]: value });
+  }
+
+  return (
+    <>
+      <Modal title={"Create User"}>
+        <FormProvider
+          formValues={formState}
+          handleInputChange={handleInputChange}
+        >
+          <form onSubmit={handleSubmit}>
+            {inputState.map(
+              ({ type, label, name, placeholder, message }, index) => (
+                <InputField
+                  key={index}
+                  type={type}
+                  label={label}
+                  name={name}
+                  placeholder={placeholder}
+                  message={message}
+                />
+              )
+            )}
+            <Button type={"submit"} text={"Create User"} />
+            <Button type={"button"} text={"Cancel"} secondary />
+          </form>
+        </FormProvider>
+      </Modal>
+    </>
+  );
+}
