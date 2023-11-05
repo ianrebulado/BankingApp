d45f6160787acebfad9041524cc3ab1f3f2bd957
TyJacalan <muzuuu@protonmail.com>
2023-11-05 15:52:28 +0800

    Create WithdrawForm

--- src/pages/AdminDashboard/Forms/WithdrawForm.jsx
+++ src/pages/AdminDashboard/Forms/WithdrawForm.jsx
@@ -1,5 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function WithdrawForm() {
-  return <div>WithdrawForm</div>;
+import { Button, Modal, InputField, FormProvider } from "../../../components/";
+import {
+  withdrawFormInputs,
+  initialWithdrawFormState,
+} from "../../../lib/constants/globals";
+import {
+  clearValidationMessages,
+  validateTransactionForm,
+} from "../../../lib/utils/validations";
+import { withdraw } from "../../../lib/utils/transactions";
+import { filterUsersByName } from "../../../lib/utils/users";
+
+export default function WithdrawForm({
+  usersData,
+  setShowModal,
+  setShowToast,
+}) {
+  const [inputState, setInputState] = useState(withdrawFormInputs);
+  const [formState, setFormState] = useState(initialWithdrawFormState);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let isValidForm = true;
+
+    clearValidationMessages(inputState, setInputState);
+
+    isValidForm = validateTransactionForm(
+      inputState,
+      setInputState,
+      { ...formState, type: "withdraw" },
+      usersData
+    );
+
+    if (isValidForm) {
+      const userId = filterUsersByName(formState.username).user_id;
+
+      withdraw(userId, formState.amount);
+
+      setShowToast(true);
+      setShowModal(false);
+    } else {
+      console.log("Form is not valid");
+    }
+  }
+
+  function handleCancel() {
+    setShowModal(false);
+  }
+
+  function handleInputChange(name, value) {
+    setFormState({ ...formState, [name]: value });
+  }
+
+  return (
+    <>
+      <Modal title={"Make a Withdrawal"}>
+        <FormProvider
+          formValues={formState}
+          handleInputChange={handleInputChange}
+        >
+          <form onSubmit={handleSubmit}>
+            {inputState.map(
+              ({ type, label, name, placeholder, message }, index) => (
+                <InputField
+                  key={index}
+                  type={type}
+                  label={label}
+                  name={name}
+                  placeholder={placeholder}
+                  message={message}
+                />
+              )
+            )}
+            <Button type={"submit"} text={"Withdraw"} />
+            <Button
+              type={"button"}
+              text={"Cancel"}
+              handleClick={handleCancel}
+              secondary
+            />
+          </form>
+        </FormProvider>
+      </Modal>
+    </>
+  );
 }
