1d19bdc5a4517ccec096dd60f627e781b753595e
cimafrancamae <cimafrancamae@gmail.com>
2023-11-05 20:57:15 +0800

    fixed modal component

--- src/components/Global/Modal/Modal.jsx
+++ src/components/Global/Modal/Modal.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { XCircle } from 'lucide-react'
 
-export default function Modal({title, children}) {
+export default function Modal({title, setShowModal, children}) {
+
+  const handleClose = () => {
+    setShowModal(false);
+  }
+  
   return (
-    <div className="modal-container">
-        <div className="modal">
-            <h1>{ title }</h1>
-            <div>{ children }</div>
-            <button id="close"> Close </button>
+    <div className="modal">
+        <div className="modal-container">
+          <div className="modal-header">
+            <div className="modal-title">  
+              <h1>{ title }</h1>
+            </div>  
+            <div className="modal-close">
+              <XCircle onClick={handleClose} />
+            </div>
+          </div>
+          <div className="modal-children">
+            { children }
+          </div>
         </div>
     </div>
   )
