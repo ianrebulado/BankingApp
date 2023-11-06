2ded906da3885d8d9f37f9f6a5b0a7e946a5448c
cimafrancamae <cimafrancamae@gmail.com>
2023-11-07 00:57:43 +0800

    fixed loginpage style

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import SignInForm from "./LoginPage/SignInForm";
-import "../styles/css/loginpage.css";
+import SignInForm from "./Forms/SignInForm";
 
 export default function LoginPage() {
   return (
@@ -8,13 +7,13 @@ export default function LoginPage() {
       <div className="login-page">
         <img className="home-img" src="/public/assets/1.jpg" />
         <div className="login-container">
-          <div className="login-form">
-            <img
-              src="/public/assets/combi-dark-logo.png"
-              alt="logo"
-              className="home-logo"
+          <img
+            src="/public/assets/combi-dark-logo.png"
+            alt="logo"
+            className="home-logo"
             />
-            <SignInForm />
+          <div className="login-form">
+            <SignInForm className={'sign-in-form'} />
           </div>
         </div>
       </div>
