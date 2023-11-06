c6dc1ae09e9b966cd8e5d58230a69ebdddffc751
TyJacalan <muzuuu@protonmail.com>
2023-11-06 19:41:26 +0800

    Create and import SearchInput

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,18 @@ import Header from "./Global/Header/Header";
 import InputField from "./Global/InputField/InputField";
 import { FormProvider } from "./Global/Form/FormContext";
 import Modal from "./Global/Modal/Modal";
+import SearchInput from "./Global/SearchInput/SearchInput";
 import Toast from "./Global/Toast/Toast";
 import Table from "./Global/Table/Table";
 
-export { Button, Card, Header, InputField, FormProvider, Modal, Toast, Table };
+export {
+  Button,
+  Card,
+  Header,
+  InputField,
+  FormProvider,
+  Modal,
+  SearchInput,
+  Toast,
+  Table,
+};
