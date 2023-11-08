b531b88ccbe1b3bea134947dff77929d0b509df6
cimafrancamae <cimafrancamae@gmail.com>
2023-11-08 11:26:14 +0800

    added toggle to sidebar items

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,18 +3,14 @@ import SidebarHeader from './SidebarHeader';
 import SidebarControl from './SidebarControl';
 import NavList from './NavList';
 
-
-function Sidebar({isOpen, navItems}) {
-   
-    return (
-        <>
-            <div className={`sidebar ${isOpen ? 'open' : ''}`}>
-                <SidebarHeader />
-                <NavList items={navItems} />
-                <SidebarControl />
-            </div>
-        </>
-    );
+function Sidebar({ isOpen, navItems, toggleSidebar }) {
+  return (
+    <div className={`sidebar ${isOpen ? "open" : ""}`}>
+      <SidebarHeader />
+      <NavList items={navItems} toggleSidebar={toggleSidebar} />
+      <SidebarControl />
+    </div>
+  );
 }
 
 export default Sidebar;
