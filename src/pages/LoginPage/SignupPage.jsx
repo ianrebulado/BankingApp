a7d959ca7687a41abec33ca3a50092707c276c62
cimafrancamae <cimafrancamae@gmail.com>
2023-11-10 01:58:59 +0800

    fixed signup page width and height

--- src/pages/LoginPage/SignupPage.jsx
+++ src/pages/LoginPage/SignupPage.jsx
@@ -11,10 +11,21 @@ export default function SignupPage() {
         <div className="login-container">
           <div className="title-container">
             <h1 className="form-title">Register</h1>
-            <img src={logo} alt="logo" className="sign-up-logo" />
+            {/* <img src={logo} alt="logo" className="sign-up-logo" /> */}
           </div>
           <div className="login-form">
             <SignUpForm className={"sign-in-form"} />
+          </div>
+          <div className="login-page-footer">
+            <span>
+              Bankwise is regulated by the Bangko Sentral ng Pilipinas. For
+              inquiries and comments, please contact our24-Hour Customer
+              Service.
+            </span>
+            <span> A proud member of BancNet and PDIC. </span>
+            <span>
+              Deposits are insured by PDIC up to PHP 500,000 per depositor.
+            </span>
           </div>
         </div>
       </div>
