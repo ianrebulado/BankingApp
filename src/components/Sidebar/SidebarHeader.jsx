2ac10bac8a49bfb637740c2bd36dde114a0af576
TyJacalan <muzuuu@protonmail.com>
2023-11-09 19:46:19 +0800

    Fixed error with login and signup page image imports

--- src/components/Sidebar/SidebarHeader.jsx
+++ src/components/Sidebar/SidebarHeader.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from '../../../public/assets/mg-light-logo.png'
+import React from "react";
+import logo from "../../../public/mg-light-logo.png";
 
 const SidebarHeader = () => {
-    return (
-        <div className='sidebar-header'>
-            <img src={logo} alt='logo' className='logo' />
-        </div>
-    );
+  return (
+    <div className="sidebar-header">
+      <img src={logo} alt="logo" className="logo" />
+    </div>
+  );
 };
 
 export default SidebarHeader;
--- src/pages/LoginPage/SignupPage.jsx
+++ src/pages/LoginPage/SignupPage.jsx
@@ -1,22 +1,18 @@
-import React from 'react'
+import React from "react";
 import SignUpForm from "./Forms/SignUpForm";
+import bgImg from "../../../public/1.jpg";
+import logo from "../../../public/combi-light-logo.png";
 
 export default function SignupPage() {
   return (
     <>
       <div className="login-page">
-        <img className="home-img" src="/public/assets/1.jpg" />
+        <img className="home-img" src={bgImg} />
         <div className="login-container">
-            <div className="title-container">
-              {/* <h1 className='form-title'>Register</h1> */}
-              <img
-                src="/public/assets/combi-dark-logo.png"
-                alt="logo"
-                className="sign-up-logo"
-              />
-            </div>
+          {/* <h1 className='form-title'>Register</h1> */}
+          <img src={logo} alt="logo" className="home-logo" />
           <div className="login-form">
-            <SignUpForm className={'sign-in-form'} />
+            <SignUpForm className={"sign-in-form"} />
           </div>
         </div>
       </div>
